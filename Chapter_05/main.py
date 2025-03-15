17ee9319fbc1172f14b5c4c4e4e6f87323f5333d
AlirezaKhajehvandi <alireza76khajehvandi@gmail.com>
2025-03-16 01:51:05 +0330

    after tutorial

--- Chapter_05/main.py
+++ Chapter_05/main.py
@@ -1,1 +1,10 @@
 ## Check the number and tell whether it is prime or not.
+
+n = 10
+
+for num in range (2, n):
+    if ((n % num) == 0):
+        print (f"{n} is not prime!")
+        break
+else:
+    print (f"{n} is prime!")
