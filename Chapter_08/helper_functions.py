ed1d60cf578396e8d14e390c2864b83625244678
AlirezaKhajehvandi <alireza76khajehvandi@gmail.com>
2025-03-22 13:46:41 +0330

    Chapter 08 published
    functions

---
+++ Chapter_08/helper_functions.py
@@ -0,0 +1,118 @@
+###############################################################
+def print_odd_numbers(n: int):
+    """
+    Prints all odd numbers from 1 to n.
+    
+    Parameters:
+    n (int): The upper limit.
+    """
+    for i in range(1, n + 1):
+        if i % 2 != 0:
+            print(i)
+
+# Example usage:
+# print_odd_numbers(10)
+# print(20*"-")
+
+
+###############################################################
+def add_integers(a: int, b: int) -> int:
+    """
+    Adds two integers and returns the result.
+    
+    Parameters:
+    a (int): The first integer.
+    b (int): The second integer.
+    
+    Returns:
+    int: The sum of a and b.
+    """
+    return int(a) + int(b)
+
+# Example usage:
+# result = add_integers(3, 5)
+# print(result) 
+# print(20*"-")
+
+###############################################################
+def add_floats(a: float, b: float) -> float:
+    """
+    Adds two floats and returns the result.
+    
+    Parameters:
+    a (float): The first float.
+    b (float): The second float.
+    
+    Returns:
+    float: The sum of a and b.
+    """
+    return float(a) + float(b)
+
+# Example usage:
+# result = add_floats(3.5, 2.5)
+# print(result)  
+# print(20*"-")
+
+###############################################################
+def factorial(n: int) -> int:
+    """
+    Calculates the factorial of a number.
+    
+    Parameters:
+    n (int): The number to calculate the factorial for.
+    
+    Returns:
+    int: The factorial of n.
+    """
+    fact = 1
+
+    if n == 0 or n == 1:
+        return 1
+    else:
+        for i in range(2, n + 1):
+            fact *= i
+        return fact
+
+# Example usage:
+# result = factorial(10)
+# print(result)
+# print(20*"-")  
+
+###############################################################
+def print_fibonacci_series(n: int):
+    """
+    Prints the Fibonacci series up to n terms.
+    
+    Parameters:
+    n (int): The number of terms in the Fibonacci series.
+    """
+    a, b = 0, 1
+    for _ in range(n):
+        print(f"{a},", end=" ")
+        a, b = b, a + b
+    print(end="\n")
+# Example usage:
+# print_fibonacci_series(10)
+# print(20*"-")
+
+###############################################################
+# Example of Recursive Function
+def factorial_recursive(n: int) -> int:
+    """
+    Calculates the factorial of a number.
+    
+    Parameters:
+    n (int): The number to calculate the factorial for.
+    
+    Returns:
+    int: The factorial of n.
+    """
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Example usage:
+# result = factorial_recursive(10)
+# print(result)
+# print(20*"-")  
---
+++ Chapter_08/main.py
@@ -0,0 +1,19 @@
+import helper_functions
+from helper_functions import add_floats
+import modules.functions as func
+
+from helper_functions import *
+
+from modules.functions import add_two_numbers as add_num
+
+
+
+result = add_floats(15, 20.5)
+result = add_floats(32.5, 0.3)
+new_result = func.add_two_numbers(5, 9)
+sum_of_num = add_num(6, 7)
+
+
+print(result)
+print(new_result)
+print(sum_of_num)
