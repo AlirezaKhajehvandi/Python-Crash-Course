0da79a53a174f93837628c58db2a11cb62229478
AlirezaKhajehvandi <alireza76khajehvandi@gmail.com>
2025-04-02 21:12:03 +0330

    after tutorial

--- Chapter_10/main.py
+++ Chapter_10/main.py
@@ -1,24 +1,10 @@
 ###############################################################
 # 
 
+print(5 / 0)
 
-print("Give me two numbers, and I'll divide them.")
-print("Enter 'q' to quit.")
+try:
+    print(5 / 0)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
 
-while True:
-    first_number = input("\nFirst number: ")
-    if first_number == 'q':
-        break
-    second_number = input("\nSecond number: ")
-    if second_number == 'q':
-        break
-    try:
-        answer = int(first_number) / int(second_number)
-    except ZeroDivisionError:
-        print("You can't divide by 0!")
-    except ValueError:
-        print("Please enter valid numbet!")
-    else:
-        print("answer will be printed here!")
-        print(answer)
-        # print(f"\nResult = {answer}")
