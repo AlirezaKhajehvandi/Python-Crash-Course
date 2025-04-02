d3e22e02999c22d059f4485cbfd434ab50bca4b1
AlirezaKhajehvandi <alireza76khajehvandi@gmail.com>
2025-04-02 18:57:14 +0330

    chapter 10 is published.
    working with files

---
+++ Chapter_11/main.py
@@ -0,0 +1,25 @@
+###############################################################
+"""
+#  Importing a single class
+"""
+# from class_modules import Car
+
+# my_new_car = Car("audi", "a4", 2024)
+# print(my_new_car.get_descriptive_name())
+
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
+
+
+
+###############################################################
+"""
+#  Importing a single class
+"""
+from class_modules import Car
+
+my_new_car = Car("audi", "a4", 2024)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
